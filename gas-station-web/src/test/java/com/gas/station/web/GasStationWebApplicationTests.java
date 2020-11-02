7cf4c2390145a8ccbb6c6d381e463a06302d2327
suchuanqi <suchuanqi2008@163.com>
2020-11-02 15:30:19 +0800

    基本骨架

---
+++ gas-station-web/src/test/java/com/gas/station/web/GasStationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gas.station.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
